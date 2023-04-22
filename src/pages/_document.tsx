b46d05509dad3d70c845cdd1a1a4a80da341b7fe
Vinicius Tengaten <vinitengaten@gmail.com>
2023-04-22 04:02:44 -0300

    initial

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className="dark:[color-scheme:dark]">
-      <Head />
+      <Head>
+        <title>Encurtador | Admin</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
