28a05b3cf303a2acd0a143a8897bcfded17b9f82
Vinicius Tengaten <vinitengaten@gmail.com>
2023-04-29 17:42:13 -0300

    .

--- src/elements/SidebarElement.tsx
+++ src/elements/SidebarElement.tsx
@@ -1,13 +1,16 @@
 import { ChevronUp, Edit } from "lucide-react"
+import Image from "next/image";
+import Logo from "../../public/logo-big-name.svg"
+
 
 function SidebarElement() {
     return (
         <div id="sidebar-container" className="lg:flex-none h-20 lg:h-full lg:w-72 bg-neutral-950 shadow-[0_0_15px_0_rgba(38,38,38,.5)] border-b lg:border-b-0 lg:border-r border-neutral-800 lg:p-6 lg:overflow-y-auto">
 
         <div className="flex lg:flex-col h-full px-6 lg:px-0 items-center lg:items-start lg:min-h-[180px]">
-          <nav className="flex-auto space-y-3">
-            <a href="#" className="flex items-center  gap-3 text-xl font-bold ">
-              <Edit /> editar links
+          <nav className="flex-auto flex items-start space-y-3 w-full">
+            <a href="/admin" className="flex items-center justify-center w-full">
+              <Image draggable={false} src={Logo} alt="logo" width={150} className="mt-2"/>
             </a>
           </nav>
 
