7e80572fd12b9d6202fb3a947fc15a01d4e1a0f7
Vinicius Tengaten <vinitengaten@gmail.com>
2023-05-14 03:01:23 -0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,44 @@
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
+import { NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 
 const routes = ["admin", "api", "favicon.svg", "_next"]
 
 export async function middleware(req: NextRequest): Promise<NextResponse> {
-  let reqPathsList = req.nextUrl.pathname.substring(1).split("/")
+	let reqPathsList = req.nextUrl.pathname.substring(1).split("/")
 
-  if (!reqPathsList[0]) {
-    return NextResponse.next();
-  }
+	if (!reqPathsList[0]) {
+		return NextResponse.next()
+	}
 
-  if (!routes.includes(reqPathsList[0])) {
-    return NextResponse.rewrite(new URL(`/api/links/redirect/${req.nextUrl.pathname.replace("/", "")}`, req.url))
-  }
+	if (!routes.includes(reqPathsList[0])) {
+		return NextResponse.rewrite(
+			new URL(
+				`/api/links/redirect/${req.nextUrl.pathname.replace("/", "")}`,
+				req.url
+			)
+		)
+	}
 
-  if (reqPathsList[0] == "admin") {
-    return await adminMiddleware(req, reqPathsList)
-  }
+	if (reqPathsList[0] == "admin") {
+		return await adminMiddleware(req, reqPathsList)
+	}
 
-  return NextResponse.next()
+	return NextResponse.next()
 }
 
-async function adminMiddleware(req: NextRequest, reqPathsList: string[]): Promise<NextResponse> {
-  const logged = true;
+async function adminMiddleware(
+	req: NextRequest,
+	reqPathsList: string[]
+): Promise<NextResponse> {
+	const logged = true
 
-  if (!logged && reqPathsList[1] != "login") {
-    return NextResponse.redirect(new URL("/admin/login", req.url))
-  }
+	if (!logged && reqPathsList[1] != "login") {
+		return NextResponse.redirect(new URL("/admin/login", req.url))
+	}
 
-  if (logged && reqPathsList[1] == "login") {
-    return NextResponse.redirect(new URL("/admin", req.url))
-  }
+	if (logged && reqPathsList[1] == "login") {
+		return NextResponse.redirect(new URL("/admin", req.url))
+	}
 
-  return NextResponse.next()
+	return NextResponse.next()
 }
