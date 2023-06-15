0984276356d57243a3524d6f7adea673b1982447
Vinicius Tengaten <vinitengaten@gmail.com>
2023-06-15 16:18:52 -0300

    .

--- src/pages/components/LinkComponent.tsx
+++ src/pages/components/LinkComponent.tsx
@@ -32,7 +32,7 @@ function LinkElement(props: {
 	}
 
 	const copyLink = () => {
-		navigator.clipboard.writeText(`https://vnci.us/${props.nome}`)
+		navigator.clipboard.writeText(`https://vnici.us/${props.nome}`)
 		props.onCopy(`Link /${props.nome} copiado!`)
 	}
 	return (
