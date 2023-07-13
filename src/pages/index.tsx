f64bb13ca4ab90a444fda41b703c679485592543
Vinicius Tengaten <vinitengaten@gmail.com>
2023-07-13 17:35:34 -0300

    altera link do discord e coloração de fontes na página inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,6 @@ import DiscordLogo from "../../public/discord-logo.svg"
 import GitHubLogo from "../../public/github-logo.svg"
 import WavingHandEmoji from "../../public/waving-hand-emoj.svg"
 import Logo from "../../public/logo-big-name.svg"
-import router from "next/router"
-
-function discordButtonClickHandler() {
-	navigator.clipboard.writeText("vnicius.")
-	showMessageBox("Discord copiado!")
-}
 
 function socialMedia() {
 	return (
@@ -52,16 +46,16 @@ function socialMedia() {
 			<a
 				href="https://github.com/baautitz"
 				target="_blank"
-				className="w-40 flex items-center p-2 gap-3 border rounded-lg border-white shadow-[0_0_6px_0_rgba(255,255,255,.5)] hover:bg-white/30 transition-all ease-in duration-100"
+				className="w-40 flex items-center p-2 gap-3 border rounded-lg border-zinc-300 shadow-[0_0_6px_0_rgba(212,212,216,.5)] hover:bg-white/30 transition-all ease-in duration-100"
 			>
 				<Image draggable={false} alt="GitHubLogo" src={GitHubLogo} width={30} />
 				GitHub
 			</a>
 
-			<button
-				type="button"
-				onClick={discordButtonClickHandler}
-				className="w-40 flex items-center p-2 gap-3 border rounded-lg border-sky-700 shadow-[0_0_8px_0_rgba(3,105,161,.5)] hover:bg-sky-700/30 transition-all ease-in duration-100"
+			<a
+				href="https://discord.com/users/404867138193457163"
+				target="_blank"
+				className="w-40 flex items-center p-2 gap-3 border rounded-lg border-sky-700 shadow-[0_0_8px_0_rgba(3,105,161,.6)] hover:bg-sky-700/30 transition-all ease-in duration-100"
 			>
 				<Image
 					draggable={false}
@@ -69,8 +63,8 @@ function socialMedia() {
 					src={DiscordLogo}
 					width={30}
 				/>
-				vnicius.
-			</button>
+				Discord
+			</a>
 		</>
 	)
 }
@@ -84,7 +78,7 @@ export default function Home() {
 
 			<div className="h-screen min-h-[450px] flex flex-col justify-center items-center bg-black text-white">
 				<div className="w-full h-full flex flex-col items-center justify-center p-6 gap-3 bg-neutral-950 border border-neutral-800 shadow-[0_0_15px_0_rgba(38,38,38,.5)] md:rounded-lg md:max-w-lg md:h-fit">
-					<div className="flex flex-col items-center gap-1">
+					<div className="flex flex-col items-center gap-1 text-zinc-300">
 						<h1 className="text-5xl font-semibold flex gap-2">
 							Olá!
 							<Image
@@ -101,7 +95,7 @@ export default function Home() {
 
 					<span className="text-neutral-500">Minhas redes socias:</span>
 
-					<div className="grid md:grid-cols-2 items-center justify-center gap-2">
+					<div className="grid md:grid-cols-2 items-center justify-center gap-2 text-zinc-200">
 						{socialMedia()}
 					</div>
 
