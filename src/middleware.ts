a5256583a62a6e870bde2cda9ffb84e0172ca109
Vinicius Bautitz Tengaten <54754165+baautitz@users.noreply.github.com>
2024-12-18 15:48:40 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
 
 	// if requested url is root ("/")
 	if (!reqPathsList[0]) {
-		return NextResponse.next();
+		return NextResponse.redirect('https://profile.vnici.us');
 	}
 
 	// if requested url path includes in routes array
